b47a6bc70d656b943a80b39463e762406904489b
balkanskiy <balkanskiy.alexey@gmail.com>
2019-09-06 22:28:04 +0300

    If Expressions finished

--- Programming Basics/if Expressions/Exercise 1/src/Task.kt
+++ Programming Basics/if Expressions/Exercise 1/src/Task.kt
@@ -1,6 +1,17 @@
 package ifExpressions1
 
-fun checkSign(number: Int): String = TODO()
+fun checkSign(number: Int): String {
+    if (number == 0) {
+        return "zero"
+    }
+
+    if (number > 0) {
+        return "positive"
+    }
+
+    return "negative"
+}
+
 
 fun main(args: Array<String>) {
     println(checkSign(-19))  // negative
--- Programming Basics/if Expressions/Exercise 2/src/Task.kt
+++ Programming Basics/if Expressions/Exercise 2/src/Task.kt
@@ -1,6 +1,6 @@
 package ifExpressions2
 
-fun abs(number: Int): Int = TODO()
+fun abs(number: Int): Int = kotlin.math.abs(number)
 
 fun main(args: Array<String>) {
     println(abs(-19))  // 19
